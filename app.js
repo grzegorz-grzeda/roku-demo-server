d0de1b26112df79224e8c3bad4b747a7976aa96c
ggrzeda <grzegorz.grzeda@schange.com>
2021-10-26 17:59:38 +0200

    Remove not used libs and add env variable for PORT selection

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var PORT = 3000
+var PORT = process.env.PORT || 3000
 var app = express();
 
 app.use(logger('dev'));
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // error handler for invalid route
 app.use(function(req, res) {
-  res.send(404)
+  res.sendStatus(404)
 });
 
 app.listen(PORT, console.log(`Listening @ port ${PORT}`))
